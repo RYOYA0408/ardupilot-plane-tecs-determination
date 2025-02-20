8181c956ef5004db11dc43b267a262d89872d053
hatnac <hatnac@mmm.muroran-it.ac.jp>
2025-02-20 11:30:50 +0900

    TurnPoint間のProportionを計算する際，TPの中心距離を基準としていたが，円軌道の接点を基準とするように変更

--- ArduPlane/commands_logic.cpp
+++ ArduPlane/commands_logic.cpp
@@ -331,6 +331,7 @@ void Plane::do_RTL(int32_t rtl_altitude_AMSL_cm)
     auto_state.next_wp_crosstrack = false;
     auto_state.crosstrack = false;
     prev_WP_loc = current_loc;
+    flex_prev_WP_loc = current_loc;
     next_WP_loc = calc_best_rally_or_home_location(current_loc, rtl_altitude_AMSL_cm);
     setup_terrain_target_alt(next_WP_loc);
     set_target_altitude_location(next_WP_loc);
--- ArduPlane/mode_rtl.cpp
+++ ArduPlane/mode_rtl.cpp
@@ -62,6 +62,7 @@ void ModeRTL::update()
 
     if (!plane.rtl.done_climb && alt_threshold_reached) {
         plane.prev_WP_loc = plane.current_loc;
+        plane.flex_prev_WP_loc = plane.current_loc;
         plane.setup_glide_slope();
         plane.rtl.done_climb = true;
     }

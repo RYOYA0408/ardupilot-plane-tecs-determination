4bde2f4fec89eaae001098c787d011a6ecfba151
hatnac <hatnac@gmail.com>
2025-03-03 22:42:08 +0900

    TurnPoint間のProportionを計算する際，TPの中心距離を基準としていたが，円軌道の接点を基準とするように変更

--- ArduPlane/altitude.cpp
+++ ArduPlane/altitude.cpp
@@ -64,7 +64,8 @@ void Plane::setup_glide_slope(void)
         // こうすることで TECS.hin の不連続な変化を避けられる。
         float bearing = current_loc.get_bearing(flex_next_WP_loc);
         float ahead_dist = auto_state.wp_distance - L1_controller.get_L1_dist();
-        Location ahead_next_WP_loc = current_loc.offset_bearing(degrees(bearing), ahead_dist);
+        Location ahead_next_WP_loc = current_loc;
+	ahead_next_WP_loc.offset_bearing(degrees(bearing), ahead_dist);
         auto_state.wp_proportion = current_loc.line_path_proportion(flex_prev_WP_loc, ahead_next_WP_loc);
     }
     TECS_controller.set_path_proportion(auto_state.wp_proportion);

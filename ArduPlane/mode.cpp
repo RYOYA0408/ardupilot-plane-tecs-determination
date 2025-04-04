38ee0abbbe8c6313344e105f7767a2b0ee021b45
hatnac <hatnac@mmm.muroran-it.ac.jp>
2025-04-04 09:50:51 +0900

    DO_LAND_STARTから一定のグライドスロープで旋回降下着陸する処理を追加

--- ArduPlane/mode.cpp
+++ ArduPlane/mode.cpp
@@ -200,9 +200,6 @@ void Mode::update_target_altitude()
                !plane.current_loc.past_interval_finish_line(plane.flex_prev_WP_loc, plane.flex_next_WP_loc)) {
         // control climb/descent rate
         plane.set_target_altitude_proportion(plane.flex_next_WP_loc, 1.0f-plane.auto_state.wp_proportion);
-
-        // stay within the range of the start and end locations in altitude
-        plane.constrain_target_altitude_location(plane.flex_next_WP_loc, plane.flex_prev_WP_loc);
     } else if (plane.target_altitude.offset_cm != 0 && 
                !plane.current_loc.past_interval_finish_line(plane.prev_WP_loc, plane.next_WP_loc)) {
         // control climb/descent rate
